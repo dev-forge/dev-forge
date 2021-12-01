eb50a00522559f1dcfbcdd3a33c3076487b0f084
savoy1211 <ryanlee250@gmail.com>
2021-11-30 19:20:18 -0800

    Set up MVC files for backend

---
+++ server/api.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const apiController = require('./controllers.js');
+const router = express.Router();
+
+// GET request
+router.get('/posts', apiController.getPosts, (req, res)=> {
+  return res.status(200).json(res.locals.posts);
+});
+
+// POST request
+router.post('/posts', apiController.sendUserPost, (req, res) => {
+  return res.sendStatus(200);
+});
+
+
+// Create new user
+// router.post('/user/:newUser', apiController.handleOAuth, apiController.createUser, (req, res) => {
+
+// })
+
+// // DELETE request
+// router.delete('/posts', apiController.postTask, (req, res) => {
+//   return res.sendStatus(200);
+// });
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+const apiRouter = require('./api');
+
+// parses requests to access req.body
+app.use(express.json());
+
+// parses incoming encoded url requests, true allows nested objects
+app.use(express.urlencoded({ extended: true }));
+
+// Api Router
+app.use('/api', apiRouter);
+
+// local error handler (404)
+app.use((req, res) => res.status(404).send('Wrong path'));
+
+// global error handler (500) for serverside internal errors
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+
+module.exports = app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
