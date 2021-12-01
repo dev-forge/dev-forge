693433f0827b7b91f211cd3f5345e4ae4ebc15d6
Quinn <quinn@Michaels-MacBook-Pro.local>
2021-12-01 10:08:52 -0800

    resolved build issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React, { createContext, useReducer } from 'react';
 import {
   BrowserRouter,
-  Routes,
+  Switch,
   Route
 } from 'react-router-dom';
 import './App.scss';
 // import Nav from './components/nav/Nav.jsx';
 import Login from './components/login/Login.jsx';
+import { initialState, reducer } from './store/reducer'
+
+export const AuthContext = createContext();
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -16,11 +19,11 @@ function App() {
       value={{state, dispatch}}>
       <BrowserRouter>
         <div className="App">
-          <Routes>
+          <Switch>
             <Route path="/login" element={<Login />} />
             {/* <Route path="/create" element={<CreateUser />} />
             <Route path="/" element={<Feed />} /> */}
-          </Routes>
+          </Switch>
         </div>
       </BrowserRouter>
     </AuthContext.Provider>
